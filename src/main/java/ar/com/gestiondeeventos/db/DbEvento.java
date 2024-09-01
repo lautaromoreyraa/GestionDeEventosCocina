6c12433545b52f61a56aa41b8e57bf6b1a6f38b4
lautaromoreyraa <lautaromoreyra144@gmail.com>
2024-09-01 10:50:34 -0300

    modificacion de db para funcionamiento de creador de archivos csv

--- src/main/java/ar/com/gestiondeeventos/db/DbEvento.java
+++ src/main/java/ar/com/gestiondeeventos/db/DbEvento.java
@@ -11,18 +11,14 @@ public class DbEvento {
 
     private static List<Evento> eventoList;
 
-    private static List<Evento> todosLosEventosDB() {
-        List<Evento> eventos = new ArrayList<Evento>();
-        eventoList.add(new Evento(UUID.randomUUID(), "Locro del 25", "Gran locro 25 de Mayo", LocalDateTime.of(2024,05,25,12,30), "El domo", 75));
-        eventoList.add(new Evento(UUID.randomUUID(), "Clase de Cocina", "Clase de cocina gratuita", LocalDateTime.of(2024,06,17,12,30), "Plaza 25 de Mayo", 20));
-        eventoList.add(new Evento(UUID.randomUUID(),"Competencia de Cocina", "Competencia de participantes amateurs", LocalDateTime.of(2024,12,15,17,00), "Av. Sarmiento 1100", 10));
-        eventoList.add(new Evento(UUID.randomUUID(), "Degustación de platos regionales", "Degustación", LocalDateTime.of(2024,07,10,18,00), "Av Las Heras 175", 35));
+    public static List<Evento> getEventoList() {
+        List<Evento> eventoList = new ArrayList<Evento>();
+        eventoList.add(new Evento(UUID.randomUUID(), "Locro del 25", "Gran locro 25 de Mayo", LocalDateTime.of(2024,05,25,12,30), "El domo", 75, 75));
+        eventoList.add(new Evento(UUID.randomUUID(), "Clase de Cocina", "Clase de cocina gratuita", LocalDateTime.of(2024,06,17,12,30), "Plaza 25 de Mayo", 20, 20));
+        eventoList.add(new Evento(UUID.randomUUID(),"Competencia de Cocina", "Competencia de participantes amateurs", LocalDateTime.of(2024,12,15,17,00), "Av. Sarmiento 1100", 10, 10));
+        eventoList.add(new Evento(UUID.randomUUID(), "Degustación de platos regionales", "Degustación", LocalDateTime.of(2024,07,10,18,00), "Av Las Heras 175", 35, 35));
 
 
         return eventoList;
     }
-
-
-
-
 }
